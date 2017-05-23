dfcc352278afd49648a74b8903bed4f02bb597e5
USRDEL <jose.tinajero@epn.edu.ec>
2017-05-22 20:45:42 -0500

    ejercicio js

---
+++ 04-JavaScript/Ejercicio/Splice.js
@@ -0,0 +1,10 @@
+/**
+ * Created by USRDEL on 22/5/17.
+ */
+
+var arregloString = ["1","2"];
+
+arregloString.push("3"); //se agrega al final
+arregloString.pop(); //se borra el ultimo
+arregloString.splice(0,1,"3");
+console.log(arregloString);
